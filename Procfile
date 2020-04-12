388c2918c3261d1a4af9ebd6c6b2c95424e5bb1b
Abhijith Nandagopal <abhijith.nandagopal24@gmail.com>
2020-04-12 15:22:32 +0530

    Django stuff

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cergen.wsgi --log-file -
--- cergen/settings.py
+++ cergen/settings.py
@@ -122,5 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'statics'),
 ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
